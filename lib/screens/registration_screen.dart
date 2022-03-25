fe175521c929d56d9f4eba3e3b0c126cca810b90
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-03-25 20:05:52 +0100

    updated

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,9 +1,9 @@
+import 'package:chat_app/const.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/component/round_button.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
-
 
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
@@ -34,22 +34,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration:
+                  TextFieldDecoration.copyWith(hintText: 'Enter your Email'),
             ),
             SizedBox(
               height: 8.0,
@@ -58,27 +44,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration:
+                  TextFieldDecoration.copyWith(hintText: 'Enter your password'),
             ),
             SizedBox(
               height: 24.0,
             ),
-            RoundButton(onPressed: (){} , title:"Register", color: Colors.blueAccent,)
+            RoundButton(
+              onPressed: () {},
+              title: "Register",
+              color: Colors.blueAccent,
+            )
           ],
         ),
       ),
