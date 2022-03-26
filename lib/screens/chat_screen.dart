c3ec03bda749d6fd7aff5d3b2cc08bb4f39f2e95
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-03-26 14:54:26 +0100

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app/const.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
 
